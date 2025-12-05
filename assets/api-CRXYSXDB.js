f8d5d59dc450f7d680948471e9ac5f42fc065ad6
Opiwe <jnkqubela@gmail.com>
2025-12-05 08:25:03 +0200

    Updates

--- assets/api-CRXYSXDB.js
+++
@@ -1,1 +0,0 @@
-import{h as r,g as a}from"./index-Cmh6JiNX.js";const s=r.create({baseURL:a(),withCredentials:!0});s.interceptors.request.use(e=>{const t=localStorage.getItem("token");return t&&(e.headers.Authorization=`Bearer ${t}`),e});export{s as a};
